0848c03d2873407cbc8344eecbdb1c4d4eaa367c
shiorisiro <71478579+shiorisiro@users.noreply.github.com>
2026-06-18 10:03:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig(({mode}) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
