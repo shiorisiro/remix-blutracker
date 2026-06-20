6e46ae05cb31931c58e377d6162363a8b6f593a4
shiorisiro <71478579+shiorisiro@users.noreply.github.com>
2026-06-20 20:02:15 +0700

    Refactor MainActivity to use Insets for padding
    Updated MainActivity to use Insets for system bars.

--- android/app/src/main/java/com/blutracker/app/MainActivity.java
+++ android/app/src/main/java/com/blutracker/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.blutracker.app;
 
 import android.os.Bundle;
 import android.view.View;
+import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
@@ -11,14 +12,14 @@ public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        
         // Edge-to-edge dengan status bar terlihat (Android 15+)
         ViewCompat.setOnApplyWindowInsetsListener(getWindow().getDecorView(), (v, insets) -> {
-            WindowInsetsCompat systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        
         // Pastikan status bar tidak fullscreen (bisa swipe untuk notification)
         WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
         controller.show(WindowInsetsCompat.Type.statusBars());
