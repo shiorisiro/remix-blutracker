b5c4abfa3b13dde17b3a48867d30a195ab162dc7
shiorisiro <sandypapz123@gmail.com>
2026-06-20 16:09:51 +0700

    fix(android): status bar visibility & camera permission

--- android/app/src/main/java/com/blutracker/app/MainActivity.java
+++ android/app/src/main/java/com/blutracker/app/MainActivity.java
@@ -1,5 +1,26 @@
 package com.blutracker.app;
 
+import android.os.Bundle;
+import android.view.View;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Edge-to-edge dengan status bar terlihat (Android 15+)
+        ViewCompat.setOnApplyWindowInsetsListener(getWindow().getDecorView(), (v, insets) -> {
+            WindowInsetsCompat systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Pastikan status bar tidak fullscreen (bisa swipe untuk notification)
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        controller.show(WindowInsetsCompat.Type.statusBars());
+    }
+}
