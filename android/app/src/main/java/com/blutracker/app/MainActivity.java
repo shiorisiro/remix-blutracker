214144b8df97c3e8f62f74fbb87f3224b5594953
shiorisiro <71478579+shiorisiro@users.noreply.github.com>
2026-06-25 06:32:47 +0700

    Add files via upload

--- android/app/src/main/java/com/blutracker/app/MainActivity.java
+++ android/app/src/main/java/com/blutracker/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.blutracker.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(GoogleAuth.class);
+    super.onCreate(savedInstanceState);
+  }
+}
